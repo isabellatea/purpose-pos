49126b53211731f1210922e5537db3d66fbf3414
Isabella Tea <tea.isabella@gmail.com>
2018-05-17 01:23:47 -0700

    removed hardcoded host

--- server/basic-server.js
+++ server/basic-server.js
@@ -8,11 +8,6 @@ var handleRequest = require('./request-handler.js');
 // ports are 8080 and 1337.
 var port = process.env.PORT || 3001;
 
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
-var ip = '127.0.0.1';
-
 // We use node's http module to create a server.
 //
 // The function we pass to http.createServer will be used to handle all
@@ -20,8 +15,8 @@ var ip = '127.0.0.1';
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
 var server = http.createServer(handleRequest.requestHandler);
-console.log('Listening on http://' + ip + ':' + port);
-server.listen(port, ip);
+console.log('Listening on http://localhost:' + port);
+server.listen(port);
 
 // To start this server, run:
 //
