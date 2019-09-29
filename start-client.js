5f3c48779eae49d00042652baa9aa15c5383ea4d
Isabella Tea <tea.isabella@gmail.com>
2019-09-29 00:13:35 -0700

    added build command

--- start-client.js
+++ start-client.js
@@ -1,3 +1,3 @@
-const args = [ 'start' ];
+const args = process.argv.slice(2);
 const opts = { stdio: 'inherit', cwd: 'client', shell: true };
-require('child_process').spawn('npm', args, opts);
+require('child_process').spawn('npm', ['run', ...args], opts);
